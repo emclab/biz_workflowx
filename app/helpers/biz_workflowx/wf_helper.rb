fc134734c2232cbeaf74ecec0e4debec146d9b0a
jc <emclab2011@gmail.com>
2014-01-15 12:41:35 -0600

    JC: added inline view code for extra fields in event_form. Refectoring on wf_helper.

--- app/helpers/biz_workflowx/wf_helper.rb
+++ app/helpers/biz_workflowx/wf_helper.rb
@@ -2,10 +2,11 @@ module BizWorkflowx
   module WfHelper
     include StateMachineLogx::StateMachineLogxHelper
     def event_action
-      @title = t('Event Form') + '-' + t(params[:controller].camelize.demodulize.titleize.singularize)
+      @title = t('Event Form') + '-' + t(params[:controller][/\/.+/].sub('/', '').titleize.singularize)  #ex Event Form-Quote
       @workflow_model_object = params[:controller].camelize.singularize.constantize.find_by_id(params[:resource_id])  
-      @workflow_result_url =   params[:wf_event].downcase + '_' + params[:controller].camelize.demodulize.tableize.singularize + '_path'
-      @erb_code = find_config_const(params[:controller].sub('/', '_')+ '_' + params[:wf_event], params[:controller].camelize.deconstantize.titleize.singularize.downcase)
+      @workflow_result_url =   params[:wf_event].downcase + '_' + params[:controller][/\/.+/].sub('/', '').tableize.singularize + '_path'  #submit_quote_path
+      @erb_code = find_config_const(params[:controller][/\/.+/].sub('/', '').singularize+ '_' + params[:wf_event], params[:controller][/.+\//].sub('/', ''))
+      #ex, ('quote_submit', 'in_quotex')
     end
 
     #wf_actions_def = find_config_const('wf_actions_def', params[:controller].camelize.deconstantize.titleize.singularize.downcase)   
@@ -53,6 +54,20 @@ module BizWorkflowx
       config_var_name = params[:controller][/\/.+/].sub('/','').singularize + '_wf_action_def' #quote_wf_action_def
       wf = Authentify::AuthentifyUtility.find_config_const(config_var_name, engine_name)
       eval(wf) if wf.present? 
+    end
+    
+    def return_open_process(model, num_of_final_state)
+      num_days = find_config_const('wf_list_open_process_in_day').to_i
+      state_array = params[:controller].camelize.singularize.constantize.workflow_spec.state_names
+      open_state, all_state = [], []
+      state_array.each do |s|
+        all_state << s.to_s
+      end
+      open_state = all_state
+      for i in 1..num_of_final_state
+        open_state = open_state - [all_state[-i]]
+      end
+      model.where(params[:controller].sub('/', '_').to_sym => {:wf_state => open_state}).where("#{params[:controller].sub('/', '_')}.created_at >= ?", num_days.days.ago)
     end
     
   end
