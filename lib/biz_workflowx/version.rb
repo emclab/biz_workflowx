e7aca60ca0955520da01aed7c0547d2ea4e0d7c0
jc <emclab2011@gmail.com>
2015-02-20 22:21:13 -0600

    JC: upgraded to rails 4.2.0

--- lib/biz_workflowx/version.rb
+++ lib/biz_workflowx/version.rb
@@ -1,3 +1,3 @@
 module BizWorkflowx
-  VERSION = "3.2.12.03"
+  VERSION = "01"
 end
--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Dummy::Application.config.secret_token = '800a3cf0a7a0b4a539af6c188919d02c529fa89a35b8eef8537e034951a30e456b4c309c50eb735a76c58fe08ef1e0ce7e6315b14256c6e08eb195b4679e5e06'
+Rails.application.config.secret_key_base = 'xxx800a3cf0a7a0b4a539af6c188919d02c529fa89a35b8eef8537e034951a30e456b4c309c50eb735a76c58fe08ef1e0ce7e6315b14256c6e08eb195b4679e5e06'
